fad2dc1c7db799891977f584fe9555a21dfb0e70
hans.zandbelt@gmail.com <hans.zandbelt@gmail.com@171b609a-3964-75fa-edf3-e7e307ef598e>
2014-11-19 20:42:34 +0000

    update to 7.2.0

--- trunk/ognl/ognltest3.sh
+++ trunk/ognl/ognltest3.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PF=/Users/zandbelt/pingfederate/pingfederate-6.6.0
+PF=/Users/hzandbelt/pingfederate/pingfederate-7.2.0
 export CLASSPATH="${PF}/pingfederate/server/default/lib/ognl.jar:${PF}/pingfederate/server/default/lib/pf-protocolengine.jar:.:${PF}/pingfederate/server/default/lib/commons-logging.jar:${PF}/pingfederate/server/default/lib/javassist.jar"
 javac ognltest3.java
 java ognltest3 $*
